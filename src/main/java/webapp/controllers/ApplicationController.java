35077c56f7f3254ad75612d67ed361d5c9b6df05
Enda Lee <>
2019-04-03 20:16:17 +0100

    first commit

---
+++ src/main/java/webapp/controllers/ApplicationController.java
@@ -0,0 +1,30 @@
+package webapp.controllers;
+
+// Import framework depenencies  - required to handle the HTTP request and send a response
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
+
+// The @ annotation identifies this as a Controller class
+@Controller
+public class ApplicationController {
+
+    // This method, index(), serves as the site index - the default page
+    // Requests for the root '/' will be handled by this method
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index(@RequestParam(name = "name", required = false, defaultValue = "") String name, Model model) { 
+        // Return some text (this will be the response back to the browser)
+        model.addAttribute("name", name);
+        return "index";
+    }
+
+    @RequestMapping(value = "/about", method = RequestMethod.GET)
+    public String about() { 
+
+        return "about";
+    }
+}
